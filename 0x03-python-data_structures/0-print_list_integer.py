bd316dd5f962744e5aa8f8ed5a5095d9947e587a
zakaria <zakaria.elmajdoubi@uit.ac.ma>
2023-12-04 12:34:06 -0800

    data

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+      """
+    Print a list of integers
+    ...
+
+    Parameters
+    ----------
+    my_list : list optional
+        The list of integers
+
+    Return:
+        None
+    """
    for item in my_list: 
     print("{:d}".format(item))

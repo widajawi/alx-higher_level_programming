a94082966670efe19b3fdd64689ab47c3aa7b7d5
zakaria <zakaria.elmajdoubi@uit.ac.ma>
2024-03-18 13:20:21 -0700

    pu

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""
+List all states from a MySQL db on localhost at port 3306
+"""
+
+from mysqlman import MySQLMan
+from MySQLdb import Error
+from sys import argv, exit, stderr
+
+
+HELP = '{} username password database'.format(argv[0])
+HOST = 'localhost'
+PORT = 3306
+
 
 if __name__ == '__main__':
-
-    import MySQLdb
-    import sys
-
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
-    rows = cur.fetchall()
-    for row in rows:
+    try:
+        mysqlman = MySQLMan(
+            connect=True,
+            user=argv[1],
+            password=argv[2],
+            database=argv[3],
+            host=HOST,
+            port=PORT,
+        )
+    except IndexError:
+        stderr.write('usage: {}\n'.format(HELP))
+        exit(2)
+    except Error as e:
+        stderr.write('{}\n'.format(e.args[1]))
+        exit(1)
+    query = "SELECT id, name FROM states ORDER BY id;"
+    results = mysqlman.query([query, ()])
+    for row in results[0]:
         print(row)
